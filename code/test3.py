0b835cadd6fb0de650fb51b99fec16a75d36ea69
jprost76 <jean.prost@insa-rouen.fr>
2019-05-01 12:10:26 +0200

    changement du test

--- code/test3.py
+++ code/test3.py
@@ -11,23 +11,27 @@ import classMap
 import scipy.ndimage
 
 #%% chargement de l'image
+name = 'maze6'
 
-#imgnb = scipy.ndimage.imread("../res/lab_big_700.png",mode='L')
-imgnb = scipy.ndimage.imread("../res/lab_big_700.png",mode='L')
+imgnb = scipy.ndimage.imread('../res/'+name+'.png',mode='L')
+
+#affichage de l'image originale
 plt.figure()
 plt.imshow(imgnb,interpolation='nearest',cmap='gray') 
 
-#metrique
+#calcul de la metrique
 W = 1./(0.001+imgnb)
 
 #%% calcul de la map distance T
 
-p0 = (561,547)
+p0 = (19,399)
 m = classMap.DistanceMap(p0,W)
 m.calculerDistance()
+T = m.distanceMap()
+np.save('../result/'+name+'.npy',T)
 
 #%% affichage de T
-T = m.distanceMap()
+
 plt.plasma()
 plt.figure()
 plt.imshow(T,interpolation='nearest')
@@ -37,18 +41,20 @@ plt.title("solution de l'équation d'Eikonal |grad(T)|=F")
 
 #%% calcul de la géodesic
 
-I,J = m.calculGeodesic((2,2),alpha=0.1,it_max=300000)
+I,J = m.calculGeodesic((398,331),alpha=0.05,it_max=300000)
 
 #%% affichage de la géodesique
 fig = plt.figure()
 ax = fig.add_subplot(111)
-#ax.imshow(T,interpolation='nearest',cmap='plasma')
+ax.imshow(T,interpolation='nearest',cmap='plasma')
 ax.imshow(imgnb,interpolation='nearest',cmap='gray')
 ax.plot(J,I,c='g',linewidth=2)
+#ax.set_ylim((T.shape[0],0))
+#ax.set_xlim((0,T.shape[1]))
 #ax.scatter(J,I,c='w',s=2)
 ax.set_title("géodesique")
 plt.show()
 
 #%%sauvegarde de T
 
-np.save('../result/maze2.npy',T)
+np.save('../result/'+name+'.npy',T)
